9d1cea168c9c4d5da83008bb45a2a2e61dab88c5
Marvyn95 <marvinluyombya@gmail.com>
2023-12-13 17:40:44 +0300

    update on the base application page

--- care_atlas/base/views.py
+++ care_atlas/base/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
 def home_page(request):
     return render(request, 'base/home_page.html')
 
 def application_page(request):
+    date_1 = datetime.datetime.now().date()
+    print(date_1)
     return render(request, 'base/application_page.html')
