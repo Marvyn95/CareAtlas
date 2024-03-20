1f9e105ecf8aba4afe39cddd4f0547a603d4881d
Marvin Luyombya <marvinluyombya@gmail.com>
2024-03-20 11:42:45 +0300

    Update settings.py

--- care_atlas/care_atlas/settings.py
+++ care_atlas/care_atlas/settings.py
@@ -86,10 +86,21 @@ WSGI_APPLICATION = 'care_atlas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'care_atlas',
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
