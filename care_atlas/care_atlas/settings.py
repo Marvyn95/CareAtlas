afb615d7bfb1e795a36aac7abd52570ad9282d79
Marvin Luyombya <marvinluyombya@gmail.com>
2024-03-20 14:08:44 +0300

    Update settings.py

--- care_atlas/care_atlas/settings.py
+++ care_atlas/care_atlas/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-((h=%jnv&ky%!um5p&!kd5o4^s&m!kqgb+dz59652k_wi^a4&0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
