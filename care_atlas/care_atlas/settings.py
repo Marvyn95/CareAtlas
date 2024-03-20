c7d037013b37aaad639f5799cd40b8a8fa477a76
Marvin Luyombya <marvinluyombya@gmail.com>
2024-03-20 09:18:53 +0300

    Update settings.py

--- care_atlas/care_atlas/settings.py
+++ care_atlas/care_atlas/settings.py
@@ -147,5 +147,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'marvinluyombya123@gmail.com'
-EMAIL_HOST_PASSWORD = 'dzhj woaz hhxf ljqd'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
